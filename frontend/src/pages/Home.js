85555fc27055102b2d2f5c370f661ceb585caab3
Nidhish Krishnan <nid.nk45@gmail.com>
2023-12-16 21:12:26 +0800

    Bugfux

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Home() {
   return (
     <div>
-      <h1>Welcome to the Home Page!</h1>
+      <h1>Welcome to the Home Page HERE!</h1>
       {/* Add any other content for your home page */}
     </div>
   );
